401b3a77e522838c8460ef1f34379f377398450c
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-06-27 15:49:53 +0100

    remove reload on connect

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -60,7 +60,6 @@ const Header = () => {
                             show={connectModal}
                             onClose={()=>{
                                 setConnectModal(false)
-                                window.location.reload()
                             }}>
                                 <Connect />
                             </Modal>
