527afaf31f386427115b9b4c80cc291fbb655355
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-11-11 19:11:18 +0100

    update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@
 const Footer = ()=> {
     return (
         <>
-            <div className="flex text-sm sticky my-5">
+            <div className="flex text-sm sticky my-15">
                 <div className="lg:w-1/4 sm:w-1/7 md:w-2/7"></div>
                 <div className="lg:w-1/2 sm:w-full grow text-center">
                     Follow us on <a className="text-blue-400"
