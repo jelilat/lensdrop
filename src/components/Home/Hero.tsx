150081b244ee1472c09d438c9f84cfae186c0f4c
Jelilat Anofiu <anofiujelailah@gmail.com>
2023-02-15 16:26:59 +0100

    fix client-side error

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -12,12 +12,14 @@ const Hero = ()=> {
                 </div>
                 <div className='lg:w-1/2 md:w-3/5 sm:w-full'>
                     <div className="text-center py-4 lg:px-4 cursor-pointer">
-                        <Link href="/dashboard"
+                        <div 
                             className="p-2 bg-gradient-to-r from-cyan-400 to-blue-400 items-center text-white leading-none lg:rounded-full flex lg:inline-flex" role="alert">
                             <div className="flex rounded-full bg-cyan-600 uppercase px-2 py-1 text-xs font-bold mr-3">New ✨</div> 
                             <div className="font-semibold mr-2 text-left flex-auto">View real-time analytics on the Lensdrop Dashboard!</div>
-                            <svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
-                        </Link>
+                            <Link href="/dashboard">
+                                <svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+                            </Link>
+                        </div>
                     </div>
                     <div className="rounded-lg border-0 my-5 content-center place-items-center">
                         <div className="py-6 mb-4 px-5 rounded-lg border-b bg-hero bg-gradient-to-r from-cyan-400 to-blue-400">
